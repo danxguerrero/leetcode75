b64c9b67f7b5ee6a6446f52125b0f17e206ac5e0
danxguerrero <danxguerrero@yahoo.com>
2024-10-10 23:34:10 -0400

    1071. Greatest Common Divisor of Strings solution

--- problems.js
+++ problems.js
@@ -53,3 +53,56 @@ var mergeAlternately = function(word1, word2) {
 
     return result
 };     
+
+// 1071. Greatest Common Divisor of Strings
+
+// For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+ 
+
+// Constraints:
+
+// 1 <= str1.length, str2.length <= 1000
+// str1 and str2 consist of English uppercase letters.
+
+// Euclidean algorithm
+var gcd = function(a, b) {
+    if (b === 0) return a
+    return gcd(b, a % b)
+}
+
+// var gcd = function(a,b) {
+//     while(b !== 0) {
+//         let temp = b
+//         b = a % b
+//         a = temp
+//     }
+
+//     return a
+// }
+
+
+var gcdOfStrings = function(str1, str2) {
+    // if they don't have the same prefix, return an empty string
+    if (str1 + str2!== str2 + str1) return ''
+     
+    // find the gcd of the two lengths
+    const gcdlength = gcd(str1.length, str2.length)
+
+    // return the substring of str1 that is the gcd of the two lengths
+    return str1.substring(0, gcdlength)
+};
