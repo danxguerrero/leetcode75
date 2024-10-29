bf407afdb51245721c992a3dab684dbe1a17ebc0
danxguerrero <danxguerrero@yahoo.com>
2024-10-28 23:15:52 -0400

    392. Is Subsequence solution

--- problems.js
+++ problems.js
@@ -477,3 +477,44 @@ var moveZeroes = function(nums) {
 
     return nums;
 };
+
+
+// 392. Is Subsequence
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+// Example 2:
+
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 100
+// 0 <= t.length <= 104
+// s and t consist only of lowercase English letters.
+ 
+
+// Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+
+var isSubsequence = function(s, t) {
+    let sPointer = 0;
+    let tPointer = 0;
+
+    while (tPointer < t.length) {
+        if (t[tPointer] === s[sPointer]) sPointer++;
+        tPointer++;
+
+        if (sPointer === s.length) return true;
+    }
+
+    return sPointer === s.length;
+};
