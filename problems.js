2447b4d0b1844ab2fd688ebcc4592f1fb01ab864
danxguerrero <danxguerrero@yahoo.com>
2024-11-26 23:49:20 -0500

    1657. Determine if Two Strings Are Close solution

--- problems.js
+++ problems.js
@@ -714,3 +714,72 @@ var findDifference = function(nums1, nums2) {
 
     return answer;
 };
+
+// 1657. Determine if Two Strings Are Close
+
+// Two strings are considered close if you can attain one from the other using the following operations:
+
+// Operation 1: Swap any two existing characters.
+// For example, abcde -> aecdb
+// Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+// For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+// You can use the operations on either string as many times as necessary.
+
+// Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "bca"
+// Output: true
+// Explanation: You can attain word2 from word1 in 2 operations.
+// Apply Operation 1: "abc" -> "acb"
+// Apply Operation 1: "acb" -> "bca"
+// Example 2:
+
+// Input: word1 = "a", word2 = "aa"
+// Output: false
+// Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+// Example 3:
+
+// Input: word1 = "cabbba", word2 = "abbccc"
+// Output: true
+// Explanation: You can attain word2 from word1 in 3 operations.
+// Apply Operation 1: "cabbba" -> "caabbb"
+// Apply Operation 2: "caabbb" -> "baaccc"
+// Apply Operation 2: "baaccc" -> "abbccc"
+ 
+
+// Constraints:
+
+// 1 <= word1.length, word2.length <= 105
+// word1 and word2 contain only lowercase English letters.
+
+var closeStrings = function(word1, word2) {
+    if (word1.length !== word2.length) return false;
+
+    const freq1 = countLetters(word1);
+    const freq2 = countLetters(word2);
+
+    const keys1 = Object.keys(freq1).sort().join('');
+    const keys2 = Object.keys(freq2).sort().join('');
+    if (keys1 !== keys2) return false;
+
+    const values1 = Object.values(freq1).sort((a,b) => a - b);
+    const values2 = Object.values(freq2).sort((a,b) => a - b);
+    if (values1.join('') !== values2.join('')) return false;
+
+    return true;
+};
+
+const countLetters = (str) => {
+    const map = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const letter = str[i];
+        map[letter] = (map[letter] || 0) + 1;
+    }
+
+    return map;
+}
